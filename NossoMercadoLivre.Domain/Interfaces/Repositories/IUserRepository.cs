c102ce44c76de13b9db1899ca8d0a56b247d5b2e
Erick Henrique Silva Nunes <erickhsn@hotmail.com>
2020-04-28 03:56:58 -0300

    Implementado cadastro de novo usuário

---
+++ NossoMercadoLivre.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using NossoMercadoLivre.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NossoMercadoLivre.Domain.Interfaces.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+
+    }
+}
